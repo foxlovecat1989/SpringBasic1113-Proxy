629cf9d0873fc4cde5aa7b2a99471e1e7db14442
Huang Kai Chen <foxlovecat1989@gmail.com>
2020-11-19 16:54:15 +0800

    Proxy - stable proxy

--- src/main/java/com/mycompany/springbasic1113/Proxy/sta/ProxyDemo.java
+++ src/main/java/com/mycompany/springbasic1113/Proxy/sta/ProxyDemo.java
@@ -13,23 +13,24 @@ class RealSubject implements Subject{
     
 }
 class Proxy implements Subject{
-    private Subject s;
+    private Subject subject;
 
-    public Proxy(Subject s) {
-        this.s = s;
+    public Proxy(Subject subject) {
+        this.subject = subject;
     }
     @Override
     public void doRequest() {
         System.out.println("Before...");    // 公用程式
-        s.doRequest();                    // 調用指定的業務程式
+        subject.doRequest();                    // 調用指定的業務程式
         System.out.println("After...");     // 公用程式
     }
 
 }
 public class ProxyDemo {
     public static void main(String[] args) {
-        Subject s = new RealSubject();
-        s.doRequest();
-        
+        // Subject s = new RealSubject();
+        // s.doRequest();
+        Proxy proxy = new Proxy(new RealSubject());
+        proxy.doRequest();
     }
 }
