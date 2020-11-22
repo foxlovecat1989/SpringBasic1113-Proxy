0965c82a10c80900ea8df1105bf1023d4a078902
Huang Kai Chen <foxlovecat1989@gmail.com>
2020-11-22 20:27:10 +0800

    Proxy - AOP

--- src/main/java/com/mycompany/springbasic1113/aop/LogAspect.java
+++ src/main/java/com/mycompany/springbasic1113/aop/LogAspect.java
@@ -2,6 +2,7 @@ package com.mycompany.springbasic1113.aop;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +11,15 @@ import org.springframework.stereotype.Component;
 @Order
 public class LogAspect {
     
+    @Pointcut(value = "execution(* com.mycompany.springbasic1113.aop.*.* (..) )")
+    public void cut(){
+    }
     // 前置通知(Advice)
     // @Before(value = "execution(public int com.mycompany.springbasic1113.aop.CalcImpl.add(int, int))") // 切入點表達式
     // @Before(value = "execution(public int com.mycompany.springbasic1113.aop.CalcImpl.*(int, int))")
     // @Before(value = "execution(public int com.mycompany.springbasic1113.aop.*.*(int, int))")
-    @Before(value = "execution(* *.*(..))")
+    //@Before(value = "execution(* *.*(..))")
+    @Before(value = "cut()") // 切入點參照
     public void before(){
         System.out.println("before...");
     }
