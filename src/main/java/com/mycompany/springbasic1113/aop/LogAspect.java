dd1ccaf16d0403e47a76cefd61da914275c6ac26
Huang Kai Chen <foxlovecat1989@gmail.com>
2020-11-22 19:58:18 +0800

    Proxy - AOP

--- src/main/java/com/mycompany/springbasic1113/aop/LogAspect.java
+++ src/main/java/com/mycompany/springbasic1113/aop/LogAspect.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 public class LogAspect {
     
     // 前置通知(Advice)
-    @Before(value = 
-            "execution(public int com.mycompany.springbasic1113.aop.CalcImpl.add(int, int))") // 切入點表達式
+    // @Before(value = "execution(public int com.mycompany.springbasic1113.aop.CalcImpl.add(int, int))") // 切入點表達式
+    @Before(value = "execution(public int com.mycompany.springbasic1113.aop.CalcImpl.*(int, int))")
     public void before(){
         System.out.println("before...");
     }
